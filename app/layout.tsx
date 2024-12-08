9594d446a769b6ad211f2e2d250ca86bbbb90e23
Antony Lambinon <78624592+Tehen1@users.noreply.github.com>
2024-12-08 16:33:51 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { AuthProvider } from '@/frontend/context/auth'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from "@vercel/analytics/react"
 import '../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
